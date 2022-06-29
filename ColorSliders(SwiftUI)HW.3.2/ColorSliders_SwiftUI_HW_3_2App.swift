122e5e2e15445daefc59146993fc2ee609e309d4
Sergey-Yurtaev <engineerk2@mail.ru>
2022-06-29 12:09:19 +0500

    Initial Commit

---
+++ ColorSliders(SwiftUI)HW.3.2/ColorSliders_SwiftUI_HW_3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliders_SwiftUI_HW_3_2App.swift
+//  ColorSliders(SwiftUI)HW.3.2
+//
+//  Created by Sergey Yurtaev on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliders_SwiftUI_HW_3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorSliders(SwiftUI)HW.3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorSliders(SwiftUI)HW.3.2
+//
+//  Created by Sergey Yurtaev on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
